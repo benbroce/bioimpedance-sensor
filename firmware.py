b5626cd50597b242af18151ad45880127a6f8ff5
Benjamin Broce <benbroce2@gmail.com>
2024-03-26 14:57:20 -0600

    Create firmware.py with work notes

---
+++ firmware.py
@@ -0,0 +1,15 @@
+"""
+
+1. Read several short samples from ADC (several wave periods each)
+
+2. Bandpass filter the sample using an IIR filter
+
+3. Find the maximum and minimum values in each sample
+
+4. Average all the maximum values and all the minimum values from all the samples
+
+5. Calculate the peak-to-peak voltage of the input at the excitation frequency
+
+6. Use this peak-to-peak voltage to calculate impedance
+
+"""
